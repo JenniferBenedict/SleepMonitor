8a2011d4f37ecc901717f6cb2959513328324a28
JenniferBenedict <jbenedict1@drew.edu>
2018-11-02 17:22:08 -0400

    Added recorder button and view stats button to home screen

--- app/src/main/java/victorine/sleepmonitor/MainActivity.java
+++ app/src/main/java/victorine/sleepmonitor/MainActivity.java
@@ -1,15 +1,45 @@
 package victorine.sleepmonitor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button stats;
+    ImageView recorder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //test
+        recorder = (ImageView) findViewById(R.id.toggleRecording);
+        stats = (Button) findViewById(R.id.statsbutton);
+        recorder.setOnClickListener(this);
+        stats.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.statsbutton){
+            Intent statsScreen = new Intent(this,Stats.class);
+            startActivity(statsScreen);
+        }
+
+
+    }
+    private void setRecorderState(boolean running) {
+        ImageView button = (ImageView) findViewById(R.id.toggleRecording);
+        if (running) {
+            button.setImageResource(R.drawable.ic_action_stop);
+        } else {
+            button.setImageResource(R.drawable.ic_action_play);
+        }
     }
 }
---
+++ app/src/main/java/victorine/sleepmonitor/Stats.java
@@ -0,0 +1,15 @@
+package victorine.sleepmonitor;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Stats extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.stats_screen);
+
+
+    }
+}
