377efee25267972843a6d9e2861c5d69bee669ee
JenniferBenedict <jbenedict1@drew.edu>
2018-11-14 12:06:19 -0500

    Added functionality to change recorder button to play or stop depending on recorder status.

--- app/src/main/java/victorine/sleepmonitor/MainActivity.java
+++ app/src/main/java/victorine/sleepmonitor/MainActivity.java
@@ -12,13 +12,14 @@ import android.widget.ToggleButton;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Button stats;
     ImageView recorder;
+    int recorderStatus = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        recorder = (ImageView) findViewById(R.id.toggleRecording);
+        recorder = (ImageView ) findViewById(R.id.toggleRecording);
         stats = (Button) findViewById(R.id.statsbutton);
         recorder.setOnClickListener(this);
         stats.setOnClickListener(this);
@@ -31,18 +32,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent statsScreen = new Intent(this,Stats.class);
             startActivity(statsScreen);
         }
+        /* Onclick changes the recorder button between
+        play and stop, based on the recorder status.
+        Will be updated when recording function is integrated.
+         */
+        if(view.getId()==R.id.toggleRecording){
+            if (recorderStatus ==0){
+                recorder.setImageResource(R.drawable.ic_action_stop);
+                recorderStatus = 1;
+            }
+            else{
+                recorder.setImageResource(R.drawable.ic_action_play);
+                recorderStatus=0;
+            }
+                        }
+        }
 
 
-    }
+
+
 
     /*will use this to change recorder button from record to stop recording
     once we implement the recording function*/
-    private void setRecorderState(boolean running) {
+    private void setRecorderState(int status) {
         ImageView button = (ImageView) findViewById(R.id.toggleRecording);
-        if (running) {
+        if (status ==0) {
             button.setImageResource(R.drawable.ic_action_stop);
+            status = 1;
         } else {
             button.setImageResource(R.drawable.ic_action_play);
+            status = 0;
         }
     }
 }
